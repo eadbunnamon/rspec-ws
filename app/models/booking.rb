8a67e4b0a560c1c2d6f311b28b01c8855b73090f
Ead Bunnamon <ead@iwa.fi>
2020-06-01 11:36:34 +0700

    Add scope in booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,10 @@ class Booking < ApplicationRecord
   validates :booking_name, presence: true
   validates :booking_number, presence: true
 
+  scope :past, -> { where("end_at::date < ?", Time.now.to_date) }
+  scope :staying, -> { where("start_at::date <= :today AND end_at::date >= :today", today: Time.now.to_date) }
+  scope :upcoming, -> { where("start_at::date > ?", Time.now.to_date) }
+
   before_create :encrypt_booking_number
 
   private
--- spec/services/encrypting_service_spec.rb
+++ spec/services/encrypting_service_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe EncryptingService, type: :helper do
-  context '#encrypt' do
+  context '.encrypt' do
     it "can encrypt text" do
       expect(EncryptingService.encrypt("secret!")).not_to eq("secret!")
     end
   end
 
-  context '#decrypt' do
+  context '.decrypt' do
     it "can decrypt text" do
-      encrypt_text = EncryptingService.encrypt("secret!")
-      expect(EncryptingService.decrypt(encrypt_text)).to eq("secret!")
+      encrypted_text = EncryptingService.encrypt("secret!")
+      expect(EncryptingService.decrypt(encrypted_text)).to eq("secret!")
     end
   end
 end
